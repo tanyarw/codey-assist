112bdfdd9a3a0bc66f468a463fcc932ec8053547
Tanya Warrier <tanyawarrier@google.com>
2024-05-03 16:35:55 +0530

    updates gitignore functionality

--- codey_assist/utilities/changed_files.py
+++ codey_assist/utilities/changed_files.py
@@ -20,10 +20,11 @@ def get_changed_files_in_dir(directory_path):
         universal_newlines=True,
     )
     changed_files = result.stdout.splitlines()
+    print("Changed files:", changed_files)
 
     os.chdir(cwd)
     select_files = [
-        os.path.join(directory_path, f.split("/")[-1])
+        os.path.join(directory_path, f)
         for f in changed_files
         if f.endswith((".py", ".js", ".md", ".html", ".ts", ".go", ".java"))
     ]
@@ -55,13 +56,22 @@ def should_ignore(file_path, ignore_patterns_dict):
     """Checks if a file path should be ignored based on the collected ignore patterns."""
 
     parent_dir = os.path.dirname(file_path)
+    # /Users/tanyawarrier/Desktop/projects/codey-assist/codey_assist/__pycache__/__init__.cpython-311.pyc
 
     for ignore_dir in ignore_patterns_dict:
         if parent_dir.startswith(ignore_dir):
+            dp = parent_dir.replace(ignore_dir, "", 1) + "/"
+
             for pattern in ignore_patterns_dict[ignore_dir]:
                 if fnmatch.fnmatch(os.path.basename(file_path), pattern):
                     return True
-                if parent_dir.startswith(os.path.join(ignore_dir, pattern)):
-                    return True
+
+                if pattern.endswith("/"):
+                    if pattern in dp:
+                        return True
+
+                if pattern.endswith("/*"):
+                    if pattern[:-1] in dp:
+                        return True
 
     return False
